fa775fa0bc6a148d7ac452eac83811dd67fdd857
Aman <amsingh714@gmail.com>
2019-03-26 15:14:33 -0400

    able to GET with specific ID

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -15,12 +15,33 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   db("zoos")
-  .then(zoos => {
-    res.status(200).json(zoos)
-  })
-  .catch(error => {
-    res.status(500).json({ error: "The zoos could not be retrieved." })
-  })
-})
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The zoos could not be retrieved." });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("zoos")
+    .where({ id })
+    .first()
+    .then(zoo => {
+      if (zoo) {
+        res.status(200).json(zoo);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The zoo with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The zoo with the specified ID could not be retrieved."
+      });
+    });
+});
 
 module.exports = router;
