4a7ec768b3a65977ad1a6ae228fd18a2dc855905
Aman <amsingh714@gmail.com>
2019-03-26 14:29:33 -0400

    initial set up complete

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const express = require('express');
-const helmet = require('helmet');
+const server = require("./api/server");
 
-const server = express();
-
-server.use(express.json());
-server.use(helmet());
-
-// endpoints here
-
-const port = 3300;
+const port = 5000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
