bc4257645cd61aaaefcc6c21543e555e689edb67
Aman <amsingh714@gmail.com>
2019-03-26 16:21:05 -0400

    able to POST bears

--- bears/bears-router.js
+++ bears/bears-router.js
@@ -17,5 +17,52 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("bears")
+    .where({ id })
+    .first()
+    .then(bear => {
+      if (bear) {
+        res.status(200).json(bear);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The bear with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The bear with the specified ID could not be retrieved."
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  const bear = req.body;
+
+  if (!bear.name) {
+    res.status(400).json({
+      error: "Please provide a name for the bear."
+    });
+  } else {
+    db("bears")
+      .insert(bear)
+      .then(ids => {
+        const id = ids[0];
+        db("bears")
+          .where({ id })
+          .first()
+          .then(bear => {
+            res.status(201).json(bear);
+          })
+          .catch(error => {
+            res.status(500).json({
+              error: "There was an error while saving the bear to the database."
+            });
+          });
+      });
+  }
+});
 
 module.exports = router;
